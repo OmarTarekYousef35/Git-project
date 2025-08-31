9b4bcb9b91529b4f4f876cea9c7dbaa2ba77880c
OmarTarekYousef35 <omaryousseff35@gmail.com>
2025-08-30 23:04:47 -0700

    add-new-comments

--- src/main.c
+++ src/main.c
@@ -5,13 +5,19 @@ double add (double a , double b){
 return a+b:
 }
 
+double sub (double a , double b){
+
+return a-b:
+}
+
 
 int void(main){
 
 int a=5;
 int b=10;
 
-printf("%d=",(a+b))
+printf("%d=",(a+b))/ submation
+printf("%d=",(a-b))/ subtraction
 
 return 0;
 
